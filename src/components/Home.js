2a615a94ec24e1f36d524739513baacb823ed7c1
Heather Barr <hnbarr@yahoo.com>
2019-07-04 17:13:03 -0500

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return(
+        <React.Fragment>
+            hello world
+            <p>home page</p>
+        </React.Fragment>
+    )
+}
+
+export default Home
