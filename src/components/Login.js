d5bcefbe9de6540e58b84068d1687dafa30452cd
Heather Barr <hnbarr@yahoo.com>
2019-07-06 17:09:31 -0500

    updated login with responsive grid layout deleted old css file and completed grid layout for dashboard

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react'
 import './styles/login.css'
-import { TextField, Button , Paper, Input, Typography} from '@material-ui/core'
+import { TextField, Button, Input, Typography} from '@material-ui/core'
 
 export default class Login extends Component {
     render() {
         return (
             <div className='loginPage'>
-                <Paper className='loginPane' id='loginLeft'>
-                    <div id='leftBox'>
-                        <Typography variant='h3' id='brand'>standup</Typography>
-                        <Typography variant='h6'> <i className="fas fa-tasks"></i> Manage your daily tasks. </Typography>
-                        <Typography variant='h6'> <i className="fas fa-folder-open"></i> Log development progress. </Typography>
-                        <Typography variant='h6'> <i className="fas fa-paperclip"></i> Take notes on projects. </Typography>
+                {/* <Paper className='loginPane' id='loginLeft'> */}
+                    <div id='detail'>
+                        <div id='leftBox'>
+                            <Typography variant='h3' id='brand'>standup</Typography>
+                            <Typography variant='h6'> <i className="fas fa-tasks"></i> Manage your daily tasks. </Typography>
+                            <Typography variant='h6'> <i className="fas fa-folder-open"></i> Log development progress. </Typography>
+                            <Typography variant='h6'> <i className="fas fa-paperclip"></i> Take notes on projects. </Typography>
+                        </div>
                     </div>
-                </Paper>
-                <Paper className='loginPane' id='loginRight'>
-                    <div id='login'>
+                {/* </Paper> */}
+                <div className='loginPane' id='loginRight'>
+                    <div id='loginSignup'>
                         <Typography variant='h5'color='primary'> Welcome back! Sign in!</Typography>
-                        <form>
+                        <form id='loginForm'>
                             <TextField id="outlined-email-input" label="Email" type="email" name="email" autoComplete="email" variant="outlined"/>
                             <TextField id="outlined-password-input" label="Password" type="password" autoComplete="current-password" variant="outlined"/>
-                            <Button variant='outlined' color='primary'>Login</Button>
+                            <div id='buttonSection'>
+                                <Button variant='outlined' color='primary'>Login</Button>
+                            </div>
                         </form>
                     </div>
-                    
                         <form id='signup'>
                             <Typography variant='h5'color='primary'>No account? Sign up now! </Typography>
                             <Input placeholder='email' type="email"></Input>
@@ -32,7 +35,7 @@ export default class Login extends Component {
                             <Input placeholder='password'type="password"></Input>
                             <Button variant='outlined' color='primary'>Sign Up!</Button>
                         </form>
-                </Paper>
+                </div>
             </div>
         )
     }
