07c985782502f51e722bcca1397261e9628b0b20
Heather Barr <hnbarr@yahoo.com>
2019-07-13 17:14:19 -0500

    made log page responsive

--- src/components/Log.js
+++ src/components/Log.js
@@ -9,12 +9,12 @@ const Log = props => {
         <div id='log'>
             <Nav id='navLog'/>
             <div id='logLeftPane'>
-                <div id='logh3'> 
+                <div id='logBar'> 
                     <h3>Logs</h3>
-                    <TextField id="formTag" label="search" placeholder='eg. mongo'/>
-                    {/* <i className="fas fa-sort"></i> */}
+                    <button className='addBtn' color='secondary'><i className="fas fa-plus-circle fa-2x"></i> </button>
                 </div>
                 <div id='logList'>
+                <TextField id="formTag" label="search" placeholder='eg. mongo'/>
                     <LogListItem title='middleware hack' date='04-20-2019' tag='express' />
                     <LogListItem title='easy router' date='02-15-2019' tag='react router' />
                     <LogListItem title='mongo steps' date='04-15-2019' tag='mongo' />
@@ -27,27 +27,30 @@ const Log = props => {
             </div>
 
             <div id='logRightPane'>
-                <LogForm />
                 <LogPreview />
             </div>
         </div>
     )
 }
 
-const LogForm = props => {
-    return (
-        <form id='addNewLog'>
-            <TextField id="formTitle" label="Title" margin="normal" variant="outlined"/>
-            <TextField id="formTag" label="Tag" margin="normal" variant="outlined" placeholder='eg. Redux'/>
-            <TextField id="formText" label="Text" placeholder="write here ..." multiline margin="normal" variant="outlined"/>
-            <Button id='formBtn' variant='outlined' color='primary'>Add New</Button>
-        </form>
-    )
-}
+// const LogForm = props => { //this will come up on modal
+//     return (
+//         <form id='addNewLog'>
+//             <TextField id="formTitle" label="Title" margin="normal" variant="outlined"/>
+//             <TextField id="formTag" label="Tag" margin="normal" variant="outlined" placeholder='eg. Redux'/>
+//             <TextField id="formText" label="Text" placeholder="write here ..." multiline margin="normal" variant="outlined"/>
+//             <Button id='formBtn' variant='outlined' color='primary'>Add New</Button>
+//         </form>
+//     )
+// }
 
 const LogListItem = props => {
+    const handleSelect = (e) => {
+        e.preventDefault()
+        console.log(e.target)
+    }
     return (
-        <div className='logItem'> 
+        <div className='logItem' onClick={handleSelect}> 
             <div id='deets'>
                 <p id='liTitle'>{props.title}</p>
                 <p id='liDate'>{props.date}</p>
@@ -58,9 +61,24 @@ const LogListItem = props => {
 }
 
 const LogPreview = props => {
+    const handleEdit = () => {
+
+    }
+    const handleDelete = () => {
+
+    }
     return (
         <div className='logPreview'>
-            have edit and delete buttons!
+            <div id='previewDetails'>
+                display name, date and tag of clicked log.  
+            </div>
+            <div id='previewTextBox'>
+                display description/body of clicked on log
+            </div>
+            <div id='previewBtns'>
+                <button onClick={handleEdit} className='edit'>edit </button>
+                <button onClick={handleDelete} className='trash'>delete</button>
+            </div>
         </div>
     )
 }
